bb1c40a2e934618fdfdc90095ba30e8f22fde236
joseesco24 <josed.escobar@outlook.com>
2022-12-24 15:29:32 -0500

    feat: first graphql funcional api

--- src/core_modules/users/routers/router.py
+++ src/core_modules/users/routers/router.py
@@ -1,38 +1,58 @@
 #!/usr/bin/env python3
 
 # ** info: python imports
+from pathlib import Path
 import logging
 
-# ** info: fastapi imports
-from fastapi import APIRouter
-from fastapi import status
+# ** info: starlette imports
+from starlette.routing import Route
 
-from fastapi import Depends
+# ** info: graphql imports
+from graphql import GraphQLSchema
+
+# ** info: ariadne imports
+from ariadne.asgi import GraphQL
+from ariadne import QueryType
+
+from ariadne.validation import cost_validator
+from ariadne import make_executable_schema
+from ariadne import load_schema_from_path
 
 # Commons
 from common_artifacts.path_utils.path_generator import path_generator
 
-# Dtos
-from src.core_modules.users.dtos.response_dto import ResponseDto
-from src.core_modules.users.dtos.request_dto import RequestDto
 
 # pylint: disable=unused-variable
-__all__: list[str] = ["users_router"]
+__all__: list[str] = [r"users_router"]
 
-users_router: APIRouter = APIRouter(prefix=path_generator.build_posix_path("users"))
+current_file_path: Path = Path(__file__).parent.resolve()
+schema_path: Path = Path(current_file_path, r"..", r"schemas", r"users.schema.graphql")
+
+schema_literal: str = load_schema_from_path(schema_path)
+
+query: QueryType = QueryType()
 
 
-@users_router.get(
-    path=path_generator.build_posix_path("data", "public"),
-    response_model=ResponseDto,
-    status_code=status.HTTP_200_OK,
+@query.field(r"first_name")
+def resolve_hello(*_):
+    logging.debug(r"resolving first_name field")
+    return r"Octavio Felipe"
+
+
+@query.field(r"second_name")
+def resolve_hello(*_):
+    logging.debug(r"resolving second_name field")
+    return r"Paz Belarcazar"
+
+
+schema_executable: GraphQLSchema = make_executable_schema(schema_literal, query)
+
+graphql_endpoint_definition: GraphQL = GraphQL(
+    validation_rules=[cost_validator(maximum_cost=5)],
+    schema=schema_executable,
+    debug=True,
 )
-async def load_configuration_file(
-    query: RequestDto = Depends(),
-) -> ResponseDto:
 
-    logging.debug("starting public data controller")
-
-    logging.debug("ending public data controller")
-
-    return ResponseDto(detail=f"{query.fileName} successfully validated")
+users_router: Route = Route(
+    path=path_generator.build_posix_path(r"users"), endpoint=graphql_endpoint_definition
+)
--- src/main.py
+++ src/main.py
@@ -8,6 +8,9 @@ from os import path
 import logging
 import sys
 
+# ** info: typing imports
+from typing import List
+
 # **info: appending src path to the system paths for absolute imports from src path
 sys.path.append(join(path.dirname(path.realpath(__file__)), "..", "."))
 
@@ -20,28 +23,35 @@ from fastapi import FastAPI
 
 # ** info: starlette imports
 from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.routing import BaseRoute
 
 # ** info: routers imports
 from core_modules.users.routers.router import users_router
 
 
 # ** info: common artifacts imports
-from common_artifacts.threading_utils.managed_daemon import managed_daemon
+# from common_artifacts.threading_utils.managed_daemon import managed_daemon
 from src.common_artifacts.logging_utils.custom_logger import custom_logger
 from src.common_artifacts.middlewares.error_handler import error_handler
 from src.common_artifacts.env_utils.env_config import env_configs
 
 # ---------------------------------------------------------------------------------------------------------------------
-# ** info: initializing app dependencies
+# ** info: initializing app routers
 # ---------------------------------------------------------------------------------------------------------------------
 
-app: FastAPI = FastAPI()
+routers: List[BaseRoute] = [users_router]
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ** info: initializing app dependencies and mounting routes
+# ---------------------------------------------------------------------------------------------------------------------
+
+app: FastAPI = FastAPI(routes=routers)
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ** info: setting up global app logging
 # ---------------------------------------------------------------------------------------------------------------------
 
-if env_configs.environment_mode == "production":
+if env_configs.environment_mode == r"production":
     custom_logger.setup_production_logging()
     logging.info(f"logger setup on {env_configs.environment_mode} mode")
 else:
@@ -49,49 +59,52 @@ else:
     logging.info(f"logger setup on {env_configs.environment_mode} mode")
 
 # ---------------------------------------------------------------------------------------------------------------------
-# ** info: setting up app routers and middlewares
+# ** info: setting up app middlewares
 # ---------------------------------------------------------------------------------------------------------------------
 
 app.add_middleware(CORSMiddleware)
-app.add_middleware(BaseHTTPMiddleware, dispatch=error_handler)
 
-app.include_router(users_router)
+app.add_middleware(BaseHTTPMiddleware, dispatch=error_handler)
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ** info: setting up app shutdown and startup subrutines
 # ---------------------------------------------------------------------------------------------------------------------
 
 
+"""
 @app.on_event("startup")
 async def startup_event() -> None:
     # pylint: disable=unused-variable
     managed_daemon.start_managed_daemon()
+"""
 
 
+"""
 @app.on_event("shutdown")
 async def shutdown_event() -> None:
     # pylint: disable=unused-variable
     managed_daemon.end_managed_daemon()
+"""
 
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ** info: disabling uvicorn access and error logs on production mode
 # ---------------------------------------------------------------------------------------------------------------------
 
-uvicorn_access: Logger = logging.getLogger("uvicorn.access")
-uvicorn_error: Logger = logging.getLogger("uvicorn.error")
+uvicorn_access: Logger = logging.getLogger(r"uvicorn.access")
+uvicorn_error: Logger = logging.getLogger(r"uvicorn.error")
 
-if env_configs.environment_mode == "production":
+if env_configs.environment_mode == r"production":
     uvicorn_access.disabled = True
     uvicorn_error.disabled = True
 else:
     uvicorn_access.disabled = False
     uvicorn_error.disabled = False
 
-if __name__ == "__main__":
+if __name__ == r"__main__":
     logging.info(f"application started in {env_configs.environment_mode} mode")
 
-if __name__ != "__main__":
+if __name__ != r"__main__":
     logging.info(f"application reloaded in {env_configs.environment_mode} mode")
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -99,17 +112,17 @@ if __name__ != "__main__":
 # ---------------------------------------------------------------------------------------------------------------------
 
 application_port: int = (
-    int(environ.get("SERVER_PORT")) if environ.get("SERVER_PORT") is not None else 10048
+    int(environ.get(r"SERVER_PORT")) if environ.get(r"SERVER_PORT") is not None else 10048
 )
 
 uvicorn_server_configs: dict[str, any] = {
-    "app": app if env_configs.environment_mode == "production" else "main:app",
-    "reload": False if env_configs.environment_mode == "production" else True,
+    "app": app if env_configs.environment_mode == r"production" else r"main:app",
+    "reload": False if env_configs.environment_mode == r"production" else True,
     "port": application_port,
-    "log_level": "warning",
+    "log_level": r"warning",
     "access_log": False,
     "use_colors": False,
-    "host": "0.0.0.0",
+    "host": r"0.0.0.0",
 }
 
 logging.info(f"application starting on port {application_port}")
@@ -118,8 +131,8 @@ logging.info(f"application starting on port {application_port}")
 # ** info: running app using the previous uvicorn asgi server settings
 # ---------------------------------------------------------------------------------------------------------------------
 
-if __name__ == "__main__":
+if __name__ == r"__main__":
     uvicorn.run(**uvicorn_server_configs)
 
-if env_configs.environment_mode == "production":
-    logging.debug("application ended")
+if env_configs.environment_mode == r"production":
+    logging.debug(r"application ended")
